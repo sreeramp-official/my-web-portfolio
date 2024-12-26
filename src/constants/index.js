fbdfad358924905b42da4ce5ed56192fe6465e99
Sreeram P <psreeram3015@proton.me>
2024-12-26 19:56:07 +0530

    Updated content in education section.

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,35 +5,17 @@ import project4 from "../assets/projects/project-4.webp";
 
 export const HERO_CONTENT = `I am an aspiring software engineer with a strong interest in data science, artificial intelligence, and software development. Currently pursuing my M.Tech in Computer Science and Engineering (AI & SE). I am eager to apply my growing knowledge to solve real-world problems, learn from experienced professionals, and contribute to innovative projects that drive progress in technology.`;
 
-export const EXPERIENCES = [
+export const EDUCATION = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "2024 - Present",
+    university: "Cochin University of Science and Technology",
+    course: "M.TECH IN COMPUTER SCIENCE AND ENGINEERING (AI & SE)"
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+    year: "2020 - 2024",
+    university: "APJ Abdul Kalam Technological University",
+    course: "B.TECH IN COMPUTER SCIENCE AND ENGINEERING"
+  }
 ];
 
 export const PROJECTS = [
