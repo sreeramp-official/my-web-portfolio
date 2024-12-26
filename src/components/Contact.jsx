d22f3a777944093ce6d62ec6d9325753e7c6414a
Sreeram P <psreeram3015@proton.me>
2024-12-26 20:31:00 +0530

    Added Contact section with icons and animations, removed unwanted borders

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,27 +1,55 @@
-import { CONTACT } from "../constants"
-import { motion } from "framer-motion"
+import { FaLocationDot } from "react-icons/fa6";
+import { CONTACT } from "../constants";
+import { motion } from "framer-motion";
+import { FaPhone } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const Contact = () => {
     return (
-        <div className='bprder-t border-stone-900 pb-20'>
-            <motion.h2 whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -100 }} transition={{ duartion: 0.5 }} className="my-10 text-center text-4xl">
+        <div className="border-t border-stone-900 pb-20">
+            <motion.h2
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 0.5 }}
+                className="my-10 text-center text-4xl"
+            >
                 Get in Touch
             </motion.h2>
 
             <div className="text-center tracking-tighter">
-                <motion.p whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duartion: 1 }} className="my-4">
-                    {CONTACT.address}
-                </motion.p>
-                <motion.p whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duartion: 1 }} className="my-4">
-                    {CONTACT.phoneNo}
-                </motion.p>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 1 }}
+                    className="my-4 flex items-center justify-center gap-2"
+                >
+                    <FaLocationDot />
+                    <span>{CONTACT.address}</span>
+                </motion.div>
 
-                <a href="" className="border-b">
-                    {CONTACT.email}
-                </a>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 1 }}
+                    className="my-4 flex items-center justify-center gap-2"
+                >
+                    <FaPhone />
+                    <span>{CONTACT.phoneNo}</span>
+                </motion.div>
+
+                <motion.a
+                    href={`mailto:${CONTACT.email}`}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 1 }}
+                    className="my-4 flex items-center justify-center gap-2"
+                >
+                    <MdEmail />
+                    <span>{CONTACT.email}</span>
+                </motion.a>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Contact
+export default Contact;
